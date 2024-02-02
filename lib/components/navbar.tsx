80264451e19e003b8bbd788901ea88fcf76bcaff
liastrauss <131149504+liastrauss@users.noreply.github.com>
2024-02-02 15:39:59 +0200

    Add Navbar component, still working on logo image

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -1,21 +1,44 @@
+'use client'
+
 import Link from 'next/link';
 import { APP_NAME, COURSE_GITHUB, DEMOS_ENABLED } from '../config';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+`;
+
+const JoinContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+`;
+
+const JoinButton = styled.button`
+  border-radius: 9px; 
+  outline: none; 
+  color: black;
+`;
 
 export default function Navbar() {
   return (
-    <div id="navbar">
-      <div className="container">
-        <div className="app-name align-center">
-          <Link href="/">{APP_NAME}</Link>
-        </div>
-        <div className="align-center flex-end">
-          <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href={COURSE_GITHUB} target="_blank">GitHub</Link></li>
-            {DEMOS_ENABLED ? <li><Link href="/demos">Demos</Link></li> : null}
-          </ul>
-        </div>
-      </div>
-    </div>
+    <NavbarContainer>
+      <JoinContainer>
+        <JoinButton>Join us!</JoinButton>
+      </JoinContainer>
+      <LogoContainer>
+        <Image src="lib\components\logo.svg" alt="Logo" width={288} height={36}/>
+      </LogoContainer>
+    </NavbarContainer>
   )
 }

72fd45a824f4b99f96713fa9a47fe8c69672dec5
liastrauss <131149504+liastrauss@users.noreply.github.com>
2024-02-11 18:26:09 +0200

    Revert "Merge branch 'navbar'"
    This reverts commit bc4bf805e1a4c359f1d6dcbbc0203f35e7bdd290, reversing
    changes made to 87f19864c646d79e374555808bf0e345fe337200.

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -1,45 +1,16 @@
 'use client'
-
-import Link from 'next/link';
-import { APP_NAME, COURSE_GITHUB, DEMOS_ENABLED } from '../config';
-import styled from 'styled-components';
 import Image from 'next/image';
-import logo from './logo.svg'
-
-const NavbarContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: black;
-`;
-
-const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 20px;
-`;
-
-const JoinContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 20px;
-`;
-
-const JoinButton = styled.button`
-  border-radius: 9px; 
-  outline: none; 
-  color: black;
-`;
+import logo from '../assets/logo.svg'
 
 export default function Navbar() {
   return (
-    <NavbarContainer>
-      <JoinContainer>
-        <JoinButton>Join us!</JoinButton>
-      </JoinContainer>
-      <LogoContainer>
+    <div id="NavbarContainer" className='flex justify-between items-center'>
+      <div id="JoinContainer" className='flex items-center'>
+        <button id="JoinButton" className='rounded-lg bg-white text-black p-2'>Join us!</button>
+        </div>
+      <div id="LogoContainer" className='flex items-center'>
         <Image src={logo} alt="Logo" width={288} height={36}/>
-      </LogoContainer>
-    </NavbarContainer>
+        </div>
+    </div>
   )
 }

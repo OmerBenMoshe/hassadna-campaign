75e929f1aec48d46e087711eb697e0524bd098b6
liastrauss <131149504+liastrauss@users.noreply.github.com>
2024-02-11 17:07:49 +0200

    Refactor navbar and website screen components with tailwind

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -6,40 +6,16 @@ import styled from 'styled-components';
 import Image from 'next/image';
 import logo from './logo.svg'
 
-const NavbarContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: black;
-`;
-
-const LogoContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 20px;
-`;
-
-const JoinContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 20px;
-`;
-
-const JoinButton = styled.button`
-  border-radius: 9px; 
-  outline: none; 
-  color: black;
-`;
 
 export default function Navbar() {
   return (
-    <NavbarContainer>
-      <JoinContainer>
-        <JoinButton>Join us!</JoinButton>
-      </JoinContainer>
-      <LogoContainer>
+    <div className='flex justify-between items-center bg-black'>
+      <div className='flex items-center p-5'>
+        <button className='rounded-full outline-none text-black'>Join us!</button>
+      </div>
+      <div className='flex items-center p-5'>
         <Image src={logo} alt="Logo" width={288} height={36}/>
-      </LogoContainer>
-    </NavbarContainer>
+      </div>
+    </div>
   )
 }
--- lib/components/website-screen.tsx
+++ lib/components/website-screen.tsx
@@ -4,29 +4,20 @@ import '@/globals.css';
 import Footer from '@/lib/components/footer';
 import Navbar from '@/lib/components/navbar';
 import type { ReactNode } from 'react';
-import styled from 'styled-components';
 
 interface Props {
   children: ReactNode[] | ReactNode;
   direction?: 'rtl' | 'ltr';
 }
 
-const HomePage = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  background-color: black;
-`;
-
 
 export default function WebsiteScreen({ direction = 'ltr', children }: Props) {
   return (
-    <HomePage>
+    <div className='flex flex-col items-center justify-center bg-black min-h-screen'>
     <div dir={direction}>
       <Navbar />
       {children}
     </div>
-    </HomePage>
+    </div>
   )
 }

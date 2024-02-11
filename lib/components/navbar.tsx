1b52d33e0c590faf9adda4ae4968c2cf544fa958
liastrauss <131149504+liastrauss@users.noreply.github.com>
2024-02-11 17:52:05 +0200

    Update background color and add Tailwind CSS without configuration

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -2,14 +2,13 @@
 
 import Link from 'next/link';
 import { APP_NAME, COURSE_GITHUB, DEMOS_ENABLED } from '../config';
-import styled from 'styled-components';
 import Image from 'next/image';
 import logo from './logo.svg'
 
 
 export default function Navbar() {
   return (
-    <div className='flex justify-between items-center bg-black'>
+    <div className='flex justify-between items-center sticky top-0'>
       <div className='flex items-center p-5'>
         <button className='rounded-full outline-none text-black'>Join us!</button>
       </div>

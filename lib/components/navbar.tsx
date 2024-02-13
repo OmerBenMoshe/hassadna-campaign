c6f14e74912f3b81c36b1181e0d1e366a322e83c
omerb <omer.benmoshe@mail.huji.ac.il>
2024-02-13 11:29:54 +0200

    Add link to "Join us!" button

--- lib/components/navbar.tsx
+++ lib/components/navbar.tsx
@@ -10,10 +10,12 @@ export default function Navbar() {
   return (
     <div className="flex justify-between items-center bg-black sticky top-0">
       <div className="flex items-center p-5">
-        <button className="rounded-full outline-none text-black">Join us!</button>
+        <Link href="/join-us">
+          <button className="rounded-full outline-none text-black">Join us!</button>
+        </Link>
       </div>
       <div className="flex items-center p-5">
-        <Image src={logo} alt="Logo" width={288} height={36}/>
+        <Image src={logo} alt="Logo" width={288} height={36} />
       </div>
     </div>
   )

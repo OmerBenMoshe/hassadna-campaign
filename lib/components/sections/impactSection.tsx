095dad7e4f2656b7e76055d12930c63d97725630
Eyal Mutzary <45517365+eyalmutzary@users.noreply.github.com>
2024-02-14 18:10:37 +0000

    Refactor impactSection

--- lib/components/sections/impactSection.tsx
+++ lib/components/sections/impactSection.tsx
@@ -7,28 +7,27 @@ const ImpactSection = () => {
       dir={"rtl"}
       className="flex flex-col justify-around h-screen px-20 text-white bg-darkGray"
     >
-      <div className="flex items-center">
-        <p className="max-w-screen-sm ml-3 text-3xl text-left">
-          בזכות ׳מפתח התקציב׳, הסדנא לידע ציבורי חשפה את נתיב המזומנים של הבית
-          היהודי:
-          <br />
-          ממשרד החינוך והסכמים קואליציוניים דרך האגף לתרבות יהודית לארגונים
-          המקורבים למפלגה.
-        </p>
-        <ArrowDownRightSVG />
+      <div className={"flex"}>
+        <div className="flex items-center w-3/5">
+          <p className="text-xs text-left md:text-2xl">
+          בזכות ׳מפתח התקציב׳, הסדנא לידע ציבורי חשפה את נתיב המזומנים של הבית היהודי: ממשרד החינוך והסכמים קואליציוניים דרך האגף לתרבות יהודית לארגונים המקורבים למפלגה.
+          </p>
+          <ArrowDownRightSVG className={"w-full mr-5"} />
+        </div>
+        <div className={"w-2/5"}></div>
       </div>
-      <div className="text-9xl">
+      <div className="text-7xl md:text-8xl">
         <span className="text-neon">* IMPACT *</span> בזכות הסדנא
       </div>
-      <div className={'flex'}>
-        <div className={'flex-1'}></div>
-        <div className="flex items-center flex-1">
-          <ArrowDownLeftSVG className={'w-full ml-5'}/>
-          <p className="text-3xl">
+      <div className={"flex"}>
+        <div className={"w-2/5"}></div>
+        <div className="flex items-center w-3/5">
+          <ArrowDownLeftSVG className={"w-full ml-5"} />
+          <p className="text-xs md:text-2xl">
             בזכות ׳ספר החוקים הפתוח׳ (בשיתוף עם עמותת ויקימדיה לישראל) פקודת
-            ״הבטריקיה האורתודוכסית, 1941" הובהרה כעדיין תקפה במאגר החקיקה הלאומי,
-            אחרי שמאגרי מידע אחרים טענו שהפקודה אינה בתקוף וכך הטעו את הפרקליטות
-            ואת בית המשפט העליון. 
+            ״הבטריקיה האורתודוכסית, 1941" הובהרה כעדיין תקפה במאגר החקיקה
+            הלאומי, אחרי שמאגרי מידע אחרים טענו שהפקודה אינה בתקוף וכך הטעו את
+            הפרקליטות ואת בית המשפט העליון. 
           </p>
         </div>
       </div>

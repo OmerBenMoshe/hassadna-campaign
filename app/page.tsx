bee24258bfab5d641b3c15c9a522d00ccacf9e66
omerb <omer.benmoshe@mail.huji.ac.il>
2024-02-16 13:06:13 +0200

    Add VolunteeringSection component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import ImpactSection from '@/lib/components/sections/impactSection';
 import WhySection from '@/lib/components/sections/whySection';
+import VolunteeringSection from '@/lib/components/sections/volunteeringSection';
 import WebsiteScreen from '@/lib/components/website-screen';
 
 
@@ -8,6 +9,7 @@ export default function Home() {
     <WebsiteScreen>
       <WhySection />
       <ImpactSection />
+      <VolunteeringSection />
     </WebsiteScreen>
   )
 }

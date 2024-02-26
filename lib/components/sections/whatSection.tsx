ef61ba281877a275ba62e46d22cb4d873cf30980
liastrauss <131149504+liastrauss@users.noreply.github.com>
2024-02-27 00:12:44 +0200

    Add new SVG components

--- lib/components/sections/whatSection.tsx
+++ lib/components/sections/whatSection.tsx
@@ -3,33 +3,39 @@
 import ClutteredDataSVG from "../svg/clutteredData";
 import WhiteArrowSVG from "../svg/whiteArrow";
 import OrganisedDataSVG from "../svg/organisedData";
+import WhiteBracketSVG from "../svg/whiteBracket";
 
 export default function WhatSection({colors}: {colors: string}) {
   return (
-    // <section className={`flex flex-col ${colors} h-[calc(100vh-88px)] transition justify-end`}></section>
-    <section className={`flex flex-col justify-center h-[calc(100vh-88px)] transition ${colors} snap-center`}>
+    <section className={`flex flex-col ${colors} h-[calc(100vh-88px)] transition snap-center`}>
       <div
         dir={"rtl"}
-        className="flex items-center mr-20 text-7xl text-bold font-primary">
-        -- מה אנחנו עושים?
+        className="flex items-center mr-10 text-7xl text-bold font-primary">
+          <span className="leading-none transform scale-x-[-1] pt-1 pr-5">
+            <WhiteBracketSVG/>
+          </span>
+          מה אנחנו עושים?
+          <span className="leading-none transform pt-1 pr-5">
+            <WhiteBracketSVG/>
+          </span>
       </div>
 
       <div 
         dir={"rtl"}
-        className="flex justify-around px-20 py-10 mx-20" 
+        className="flex justify-around py-10 mx-10" 
         >
           <div className="flex flex-col items-center"> 
             <p className="text-lg">
               בגדול, אנחנו מקבלים נתונים ממוסדות ציבוריים...
             </p> 
-            <div className="flex my-2">
+            <div className="flex my-7">
               <ClutteredDataSVG width={'20vw'} height={'20vw'}/>
             </div>  
           </div>
-          <div className="flex items-center mb-20">
+          <div className="flex items-center mb-10">
             <WhiteArrowSVG/>
           </div>
-          <div className="flex flex-col items-center my-6">
+          <div className="flex flex-col items-center">
             <div className="flex my-2">
               <OrganisedDataSVG width={'20vw'} height={'20vw'}/>
             </div>
---
+++ lib/components/svg/whiteBracket.tsx
@@ -0,0 +1,20 @@
+const WhiteBracketSVG = ({ ...rest }) => (
+    <svg
+      width="35" //59
+      height="52" //87
+      viewBox="0 0 59 87"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...rest}
+    >
+      <path
+        id="Vector"
+        d="M44 72L15 43.502L44 15"
+        stroke="#FCFCF8"
+        strokeWidth="20"
+        strokeLinecap="square"
+      />
+    </svg>
+  );
+  
+  export default WhiteBracketSVG;

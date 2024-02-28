0763621a35bc47258da600dc3823ac9e7ff3c448
liastrauss <131149504+liastrauss@users.noreply.github.com>
2024-02-28 16:47:44 +0200

    Refactor impactSection component and tailwind.config.js, added marquee effect

--- lib/components/sections/impactSection.tsx
+++ lib/components/sections/impactSection.tsx
@@ -7,34 +7,56 @@ const ImpactSection = ({colors}: {colors: string}) => {
       dir={"rtl"}
       className={`flex flex-col justify-around h-[calc(100vh-88px)] px-20 transition snap-center ${colors}`}
     >
-      <div className={"flex"}>
-        <div className="flex flex-col w-0.65 bg-white text-darkGray p-8 rounded-40">
+      <div className={"flex mr-12"}>
+        <div className="flex flex-col w-0.65 bg-white text-darkGray p-8 rounded-40 relative">
           <p className="text-lg">
             <span className="text-orange">
               &apos;מפתח התקציב&apos; &nbsp;
             </span>
           חשף את נתיב המזומנים של הבית היהודי: ממשרד החינוך והסכמים קואליציוניים דרך האגף לתרבות יהודית לארגונים המקורבים למפלגה.
           </p>
-          <a href="https://www.youtube.com/watch?v=zBGLQ9FwWtY&ab_channel=Nirshman%27sPR"
-            className="text-left text-darkGray hover:underline">
+          <a href="https://www.youtube.com/watch?v=zBGLQ9FwWtY&ab_channel=Nirshman%27sPR" target="_blank"
+            className="text-left text-darkGray hover:underline hover:decoration-orange decoration-2">
             <br/>
             אפילו עשו על זה כתבה ב&apos;חדשות 12&apos; &gt;&gt;&gt;
           </a>
+          <p className="bg-neon text-darGray text-bold rounded-full px-8 py-4 text-lg absolute -bottom-6 -right-12">
+              חשיפת שחיתות
+        </p>
         </div>
+        
         <div className={"w-0.35"}></div>
       </div>
-      <div className="text-7xl md:text-8xl font-primary">
-        <span className="text-neon">* IMPACT *</span> בזכות הסדנא
+      
+      <div className="text-7xl md:text-7.5xl font-primary relative flex">
+        <div className="animate-marquee whitespace-nowrap">
+        &nbsp;<span className="text-neon"> &nbsp;* IMPACT * </span>&nbsp; בזכות הסדנא 
+        </div>
+        <div className="absolute top-0 animate-marquee2 whitespace-nowrap">
+        &nbsp;<span className="text-neon"> &nbsp;* IMPACT * </span>&nbsp; בזכות הסדנא 
+        </div>
       </div>
-      <div className={"flex"}>
-        <div className={"w-2/5"}></div>
-        <div className="flex items-center w-3/5">
-          <p className="text-xs md:text-2xl">
-            בזכות ׳ספר החוקים הפתוח׳ (בשיתוף עם עמותת ויקימדיה לישראל) פקודת
-            &quot;הבטריקיה האורתודוכסית, 1941&quot; הובהרה כעדיין תקפה במאגר החקיקה
-            הלאומי, אחרי שמאגרי מידע אחרים טענו שהפקודה אינה בתוקף וכך הטעו את
-            הפרקליטות ואת בית המשפט העליון. 
+
+
+      <div className={"flex mr-12"}>
+        <div className={"w-0.35"}></div>
+
+        <div className="flex flex-col w-0.65 bg-white text-darkGray p-8 rounded-40 relative">
+          <p className="text-lg">
+            הודות ל-
+            <span className="text-orange">
+              &apos;ספר החוקים הפתוח&apos; &nbsp;
+            </span>
+          (בשיתוף עם עמותת ויקימדיה לישראל) פקודת &quot;הבטריקיה האורתודוכסית, 1941&quot; הובהרה כעדיין תקפה במאגר החקיקה הלאומי, אחרי שמאגרי מידע אחרים טענו שהפקודה אינה בתוקף וכך הטעו את הפרקליטות ואת בית המשפט העליון.
           </p>
+          <a href="https://www.facebook.com/openlawil/posts/3073291326247722/" target="_blank"
+            className="text-left text-darkGray hover:underline hover:decoration-orange decoration-2">
+            <br/>
+            קראו עוד על הידיעה בפייסבוק &gt;&gt;&gt;
+          </a>
+          <p className="bg-purple text-white text-bold rounded-full px-8 py-4 text-lg absolute -bottom-5 -right-12">
+              חיזוק בני מיעוט
+        </p>
         </div>
       </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,20 @@ module.exports = {
         '0.65': '65%',
         '0.35': '35%'
       },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
     colors: {
       orange: "#FE7833",

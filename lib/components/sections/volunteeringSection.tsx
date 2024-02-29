7eb16466f33dfbf32241c54c4a66dab0bc26e594
omerb <omer.benmoshe@mail.huji.ac.il>
2024-02-29 14:45:11 +0200

    initial scaling fix

--- lib/components/sections/volunteeringSection.tsx
+++ lib/components/sections/volunteeringSection.tsx
@@ -7,7 +7,8 @@ import Notification from "../Backdrop/Notification";
 import { useInView } from 'react-intersection-observer';
 
 
-const VolunteeringSection = ({colors} : {colors: string}) => {
+
+const VolunteeringSection = ({ colors }: { colors: string }) => {
   const [modalOpen, setModalOpen] = useState(false);
   const [activeField, setActiveField] = useState('' as String);
 
@@ -41,16 +42,16 @@ const VolunteeringSection = ({colors} : {colors: string}) => {
 
   const fieldColor: fieldColorType = {
     'עיצוב גרפי': 'bg-darkGray',
-    'ניהול פרויקט': 'bg-orange',
-    'ניהול מערכות מידע': 'bg-neon',
+    'ניהול פרויקט': 'bg-orange text-black',
+    'ניהול מערכות מידע': 'bg-neon text-black',
     'עיצוב חווית משתמש': 'bg-darkGray',
     'אינפוגרפיקה': 'bg-purple',
-    'ניהול מוצר': 'bg-orange',
+    'ניהול מוצר': 'bg-orange text-black',
     'ניתוח נתונים': 'bg-purple',
     'קופירייטינג': 'bg-darkGray',
-    'עיצוב קמפיינים': 'bg-orange',
-    'פיתוח תוכנה': 'bg-neon',
-    'פיתוח ל - WEB': 'bg-neon'
+    'עיצוב קמפיינים': 'bg-orange text-black',
+    'פיתוח תוכנה': 'bg-neon text-black',
+    'פיתוח ל - WEB': 'bg-neon text-black'
   };
 
   type FieldPosition = {
@@ -63,31 +64,31 @@ const VolunteeringSection = ({colors} : {colors: string}) => {
   };
 
   const positions: Positions = {
-    'עיצוב גרפי': { top: '80%', left: '11.5%' },
-    'ניהול פרויקט': { top: '100%', left: '20%' },
-    'ניהול מערכות מידע': { top: '84%', left: '24.1%' },
-    'עיצוב חווית משתמש': { top: '75%', left: '33.7%' },
-    'אינפוגרפיקה': { top: '28%', left: '36.4%' },
-    'ניהול מוצר': { top: '100%', left: '40.7%' },
-    'ניתוח נתונים': { top: '85%', left: '53%' },
-    'קופירייטינג': { top: '61.5%', left: '58%' },
-    'עיצוב קמפיינים': { top: '81%', left: '68%' },
-    'פיתוח תוכנה': { top: '100%', left: '63.5%' },
-    'פיתוח ל - WEB': { top: '64.8%', left: '45%' },
+    'עיצוב גרפי': { top: '47.5%', left: '14.3%' },
+    'ניהול פרויקט': { top: '91%', left: '15%' },
+    'ניהול מערכות מידע': { top: '74%', left: '23%' },
+    'עיצוב חווית משתמש': { top: '57.5%', left: '33.7%' },
+    'אינפוגרפיקה': { top: '22.5%', left: '34.9%' },
+    'ניהול מוצר': { top: '91%', left: '45%' },
+    'ניתוח נתונים': { top: '78%', left: '58%' },
+    'קופירייטינג': { top: '34.5%', left: '65%' },
+    'עיצוב קמפיינים': { top: '62%', left: '73%' },
+    'פיתוח תוכנה': { top: '91%', left: '70%' },
+    'פיתוח ל - WEB': { top: '46%', left: '48%' },
   };
 
 
   type fieldRotations = { [key: string]: number };
 
   const rotations: fieldRotations = {
-    'עיצוב גרפי': -27,
+    'עיצוב גרפי': -45,
     'ניהול פרויקט': 0,
     'ניהול מערכות מידע': 30,
-    'עיצוב חווית משתמש': 10,
-    'אינפוגרפיקה': -33,
+    'עיצוב חווית משתמש': 7,
+    'אינפוגרפיקה': -5,
     'ניהול מוצר': 0,
-    'ניתוח נתונים': -20,
-    'קופירייטינג': 5.5,
+    'ניתוח נתונים': -33,
+    'קופירייטינג': -5,
     'עיצוב קמפיינים': 0,
     'פיתוח תוכנה': 0,
     'פיתוח ל - WEB': 27
@@ -104,49 +105,44 @@ const VolunteeringSection = ({colors} : {colors: string}) => {
 
 
   return (
-    <section className={`flex flex-col items-start justify-around h-[calc(100vh-88px)] overflow-x-hidden transition snap-center ${colors}`}>
+    <section className={`flex flex-col items-start justify-around h-[calc(100vh-88px)] overflow-x-hidden overflow-y-hidden transition snap-center ${colors}`}>
       <div dir={"rtl"} className="flex items-center justify-center space-x-20">
-        <div className="inline-block pl-20 text-8xl text-bold font-primary">
-          # תחומי התנדבות
+        <div className="inline-block pl-20 text-8xl text-bold font-primary whitespace-nowrap">
+          &lt; תחומי התנדבות&gt;
         </div>
         <div className="flex flex-col justify-center">
-          <span className="text-xl">
-            רואים את עצמכם באחד מהתחומים
+          <span className="text-l">
+            הסנדא לידע ציבורי נעזרת במתנדבות ומתנדבים מתחומים רבים על מנת לקדם את מטרת המיזם,<br/>ונותנת להם כלים וניסיון שיעזרו להם.ן בעולם האמיתי!
           </span>
           <span className="text-xl">
-            בתגיות למטה? לחצו עליה כדי לראות
-          </span>
-          <span className="text-xl">
-            איפה צריכים אתכם:
+            &lt;&lt;&lt; לחצו על התגיות כדי לגלות איך אפשר לעזור
           </span>
         </div>
       </div>
-      <div ref={ref} className="relative flex flex-wrap items-center w-full m-0 h-1/2">
-        {volunteerFields.map((field, index) => {
-          const x = Math.random() * 50 - 25;
+      <div id="dropping-items" ref={ref} className="relative flex flex-wrap items-center w-[1200px] h-[224px] pb-[height_of_tallest_child] m-0  mx-auto left-0 right-0">        {volunteerFields.map((field, index) => {
+        const x = Math.random() * 50 - 25;
 
-          const position = positions[field];
-          // Set a fixed width for each element
-          const width = 250;
+        const position = positions[field];
+        // Set a fixed width for each element
 
-          return (
-            <motion.div
-              className="absolute"
-              style={{ top: position.top, left: position.left, width }}
-              key={index}
-              variants={containerVariants(x, rotations[field])}
-              initial="hidden"
-              animate={inView ? 'visible' : 'hidden'}
-            >
-              <VolunteeringContainer
-                modalOpen={modalOpen}
-                open={open}
-                close={close}
-                volunteerField={field}
-                fieldColor={fieldColor[field]} />
-            </motion.div>
-          );
-        })}
+        return (
+          <motion.div
+            className="absolute p-0 m-0 "
+            style={{ top: position.top, left: position.left }}
+            key={index}
+            variants={containerVariants(x, rotations[field])}
+            initial="hidden"
+            animate={inView ? 'visible' : 'hidden'}
+          >
+            <VolunteeringContainer
+              modalOpen={modalOpen}
+              open={open}
+              close={close}
+              volunteerField={field}
+              fieldColor={fieldColor[field]} />
+          </motion.div>
+        );
+      })}
       </div>
       <hr className="w-full border-t-2 border-black" />
       <AnimatePresence
